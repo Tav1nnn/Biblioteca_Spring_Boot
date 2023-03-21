dac76b056651ba16db83bb58f24b5bddf0ca0d86
Otavio Marques <marquesotavio251204@gmail.com>
2023-03-21 16:00:59 -0300

    entitie Livro

--- backend/biblioteca/src/main/java/com/otavio/biblioteca/entities/Livro.java
+++ backend/biblioteca/src/main/java/com/otavio/biblioteca/entities/Livro.java
@@ -1,9 +1,78 @@
 package com.otavio.biblioteca.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "livros")
 public class Livro implements Serializable{
-	private static final long serialVersion = 1L;
+	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	private String nome;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant created_At;
+	
+	public Livro() {
+		
+	}
+
+	public Livro(Long id, String nome, Instant created_At) {
+		this.id = id;
+		this.nome = nome;
+		this.created_At = created_At;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Instant getCreated_At() {
+		return created_At;
+	}
+
+	public void setCreated_At(Instant created_At) {
+		this.created_At = created_At;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
 }
