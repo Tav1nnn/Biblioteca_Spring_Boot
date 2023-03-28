c6953a68be48d452fe3df53eb08a0ed3d3a3d9fd
Otavio Marques <marquesotavio251204@gmail.com>
2023-03-28 18:57:55 -0300

    entidade autor

---
+++ backend/biblioteca/src/main/java/com/otavio/biblioteca/entities/Autor.java
@@ -0,0 +1,142 @@
+package com.otavio.biblioteca.entities;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="autores")
+public class Autor implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	private String nome;
+	private String email;
+	private String numero;
+	private int cpf;
+
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+	
+	public Autor() {
+		
+	}
+
+	public Autor(Long id, String nome, String email, String numero, int cpf, LocalDateTime createdAt,
+			LocalDateTime updatedAt) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.numero = numero;
+		this.cpf = cpf;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+	}
+
+	@ManyToMany
+	@JoinTable(
+		name = "autor_livro",
+		joinColumns = @JoinColumn(name = "autor_id"),
+		inverseJoinColumns = @JoinColumn(name = "livro_id")
+	)
+	
+	Set<Livro> livros = new HashSet<>();
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	
+
+	public Set<Livro> getLivros() {
+		return livros;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Autor other = (Autor) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
